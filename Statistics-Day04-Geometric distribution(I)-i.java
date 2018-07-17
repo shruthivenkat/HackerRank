47b6f67adfe753ec23013def47184b23f5bfeefa
shruthivenkat <amritashruthi@gmail.com>
2018-07-17 13:50:22 +0530

    Add files via upload

---
+++ Statistics-Day04-Geometric distribution(I)-i.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+     public static double  DisGeo (int  x , double p  ) 
+     {
+        p =  (Math.pow((1-p), (x-1))) * p;
+        return p ;
+     }
+
+    public static void main(String[] args) {
+
+         Scanner scan = new Scanner(System.in);              
+         int n = scan.nextInt();
+         int f = scan.nextInt();
+         int x  = scan.nextInt();
+         scan.close();
+         double p = (double)n/f;
+         double resultadoGeo  =DisGeo (x, p );
+         System.out.println( Math.round((resultadoGeo )*1000.0)/1000.0 );
+    }
+
+}
